9b68ad2616b439002ff88ea38168be29ceafed92
Adharsh S <adharshs@qburst.com>
2018-08-06 16:01:33 +0530

    fixed slider autoplay

--- slider.js
+++ slider.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+var flag = false; 
+var timeOut;
 
 function requestInfo() {
     var json;
@@ -33,10 +35,14 @@ function plusSlides(n) {
 }
 
 function currentSlide(n) {
+    flag = true;
     showSlides(slideIndex = n);
+    flag = false;
+    clearTimeout(timeOut);
+    showSlides(slideIndex);    
 }
 
-function showSlides(n) {
+function showSlides(n) {    
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
@@ -49,8 +55,11 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block"; 
-    dots[slideIndex-1].className += " active";  
-    setTimeout(showSlides.bind(this, ++slideIndex), 4000);     
+    dots[slideIndex-1].className += " active";
+    if(!flag) {
+        timeOut = setTimeout(showSlides.bind(this, ++slideIndex), 4000);
+        flag = false;
+    }           
 }
 
 window.onload = requestInfo;
